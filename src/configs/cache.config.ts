0d9d82795ff3cd36891882057dc1b3bd4b4b61e7
gilberto lopes <developer.gilberto@gmail.com>
2026-03-30 15:02:05 -0300

    chore: update cache config for Shopee tokens

--- src/configs/cache.config.ts
+++ src/configs/cache.config.ts
@@ -1,7 +1,11 @@
-export const cache = {
-  ROUTE_ME_TTL: 60 * 5, // 5 minutos
+import { constants } from './constants.config';
 
-  ROUTE_ME_KEY: (userId: string) => `user:me:${userId}`,
+export const cache = {
+  userMeTTL: 60 * 5, // 5 minutos
+  userMeKey: (userId: string) => `user:me:${userId}`,
+
+  shopeeAccessTokenTTL: constants.ONE_HOUR_IN_SECONDS * 3, // 3 horas
+  shopeeAccessTokenKey: (shopId: string) => `shopee:access_token:${shopId}`,
 };
 
 export type Cache = typeof cache;
--- src/configs/constants.config.ts
+++ src/configs/constants.config.ts
@@ -1,14 +1,14 @@
-import { rawEnv } from './env.config';
-
 export const constants = {
   SALT_PASSWORD: 12,
 
-  TOKEN_EXP_IN_PLAIN_TEXT: rawEnv.EMAIL_TOKEN_EXP_IN_PLAIN_TEXT,
+  EMAIL_TOKEN_EXP_IN_PLAIN_TEXT: '30 minutos',
 
   APPLICATION_NAME: 'ShopixTurbo',
 
   MAIL_QUEUE: 'mail',
   SEND_VERIFICATION_EMAIL_JOB: 'send-verification-email',
+
+  ONE_HOUR_IN_SECONDS: 3600,
 };
 
 export type Constants = typeof constants;
