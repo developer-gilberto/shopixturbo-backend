e0af2b137daeaa7da22285717761dd6dfe9fe6ed
gilberto lopes <developer.gilberto@gmail.com>
2026-04-08 01:27:28 -0300

    chore: update cache config comments

--- src/configs/cache.config.ts
+++ src/configs/cache.config.ts
@@ -1,10 +1,10 @@
 import { constants } from './constants.config';
 
 export const cache = {
-  userMeTTL: 60 * 5, // 5 minutos
+  userMeTTL: 60 * 5, // 5 minutos em segundos
   userMeKey: (userId: string) => `user:me:${userId}`,
 
-  shopeeAccessTokenTTL: constants.ONE_HOUR_IN_SECONDS * 3, // 3 horas
+  shopeeAccessTokenTTL: constants.ONE_HOUR_IN_SECONDS * 3, // 3 horas em segundos
   shopeeAccessTokenKey: (shopId: string) => `shopee:access_token:${shopId}`,
 };
 
