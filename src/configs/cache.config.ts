332c442c4f78b13de8549ac2052aa00d055b124e
gilberto lopes <developer.gilberto@gmail.com>
2026-04-11 00:00:43 -0300

    refactor: update cache keys to include userId

--- src/configs/cache.config.ts
+++ src/configs/cache.config.ts
@@ -5,10 +5,10 @@ export const cache = {
   userMeKey: (userId: string) => `user:me:${userId}`,
 
   shopeeAccessTokenTTL: constants.ONE_HOUR_IN_SECONDS * 3, // 3 horas em segundos
-  shopeeAccessTokenKey: (shopId: string) => `shopee:access_token:${shopId}`,
+  shopeeAccessTokenKey: (userId: string, internalShopId: string) => `shopee:access_token:${userId}:${internalShopId}`,
 
   shopeeShopFullTTL: constants.ONE_HOUR_IN_SECONDS,
-  shopeeShopFullKey: (shopId: string) => `shopee:full_shop:${shopId}`,
+  shopeeShopFullKey: (userId: string, shopId: string) => `shopee:full_shop:${userId}:${shopId}`,
 };
 
 export type Cache = typeof cache;
